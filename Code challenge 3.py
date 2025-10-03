13f495bd8100a9b8b171bad4d985eef757ef9bb9
mitsuienzo1233-netizen <mitsuienzo1233@gmail.com>
2025-10-03 18:58:31 +0800

    Code challenge 3.py

---
+++ Code challenge 3.py
@@ -0,0 +1,13 @@
+p1 = input(" type anything: " ) 
+p2 = input(" type anything: " )
+p3 = input(" type anything: " )
+p4 = input(" type anything: " )
+p5 = input(" type anything: " )
+p6 = input(" type anything: " )
+p7 = input(" type anything: " )
+p8 = input(" type anything: ")
+p9 = input(" type anything: ")
+p10 = input(" type anything: ")
+
+answer = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10  
+print(" Mga kapatid ",answer)
