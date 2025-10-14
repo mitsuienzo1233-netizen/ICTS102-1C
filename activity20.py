37257c8754d5e4abda7e4aee19d490772f41ebd1
mitsuienzo1233-netizen <mitsuienzo1233@gmail.com>
2025-10-14 13:35:32 +0800

    activity20.py

---
+++ activity20.py
@@ -0,0 +1,9 @@
+its_dirts = True
+while its_dirts == True:
+    its_wash = input("continue to wash (yes/no)").lower()
+    if its_wash == "yes":
+        print("there cotinue to wash")
+        continue
+    else:
+        print("stop the wash")
+        break
